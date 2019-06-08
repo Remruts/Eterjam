e50aa3d32081a9754a8b8878b464ebf0e164fa3e
Dagian <fjprevi@gmail.com>
2019-06-08 17:17:27 -0300

    ventilador script

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -13,14 +13,44 @@ public class ProjectileScript : MonoBehaviour
     {
         initialTime = Time.time;
         lifeSpam = 3.0f;
+        theBody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time - initialTime > lifeSpam)
+        if (Time.time - initialTime > lifeSpam)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("fan"))
+        {
+            Vector3 velocity = theBody.velocity;
+            Vector3 newDirection = new Vector3();
+            float rand = Random.Range(0.0f,1.0f);
+            float sameDirectionProbability = 0.5f;
+            float multiplier = 3.0f * Random.Range(0.0f, 1.0f) + 1.0f;
+            if (rand < sameDirectionProbability)
+            {
+
+                newDirection.x = Random.Range(0f,50f) * -1.0f;
+                newDirection.y = Random.Range(0f, 50f) * -1.0f;
+                newDirection.z = velocity.z;
+            }
+            else
+            {
+
+                newDirection.x = Random.Range(0f, 50f);
+                newDirection.y = Random.Range(0f, 50f) * -1.0f;
+                newDirection.z = velocity.z;
+            }
+
+            theBody.velocity = newDirection;
         }
     }
 }
