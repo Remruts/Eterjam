57b9f1a0dfc5f3be3dfceebe4eaef1d24d56267a
m4r70 <martinjaltman@gmail.com>
2019-06-08 16:16:54 -0300

    dos jugadores

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,17 +9,24 @@ public class PlayerScript : MonoBehaviour
 
     public float speed = 5f;
     public float maxSpeed = 5f;
-    public float projectileSpeed = 300f;    
+    public float projectileSpeed = 300f;
     public float jumpForce = 300f;
+
+    public float maxCooldown = 1f;
+
     bool canJump = false;
     public LayerMask solidMask;
 
     public GameObject projectilePrefab;
     public int team = 0;
 
+    public int id = 0;
+
     public float flickSpeed = 1f;
 
     Vector2 realFlick = Vector2.zero;
+
+    float cooldown = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,41 +37,46 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float movement = Input.GetAxis("P1Horizontal");
-        if (Mathf.Abs(movement) > 0.5)
+        move();
+        jump();
+        if (cooldown == 0f) {
+            shoot();
+        } else if (cooldown > 0f)
         {
-            RaycastHit2D hit = Physics2D.Raycast(rb.transform.position, new Vector2(movement, 0), 1f, solidMask.value);
-            if (!hit)
+            cooldown -= Time.deltaTime;
+            if (cooldown < 0f)
             {
-                Debug.Log("podemos movernos");
-                rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+                cooldown = 0f;
             }
-            
         }
-
+        
         if (Mathf.Abs(rb.velocity.x) > maxSpeed)
             rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
 
-        if (canJump)
-        {
-            if (Input.GetButton("P1Jump"))
-            {                
-                rb.AddForce(new Vector2(0f, jumpForce));
-                canJump = false;
-            }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.CompareTag("piso")) {
+            canJump = true;
         }
+    }
 
-        Vector2 flick = new Vector2(Input.GetAxisRaw("P1FlickX"), -Input.GetAxisRaw("P1FlickY"));        
+    void shoot()
+    {
+        // Shooting
+        Vector2 flick = new Vector2(Input.GetAxisRaw("P" + id.ToString() +"FlickX"), -Input.GetAxisRaw("P" + id.ToString() + "FlickY"));
 
-        if (isHolding){
+        if (isHolding)
+        {
             if (flick.magnitude > 0.1f)
-            {             
+            {
                 realFlick += flick * flickSpeed * Time.deltaTime;
                 realFlick.x = Mathf.Clamp(realFlick.x, -1f, 1f);
-                realFlick.y = Mathf.Clamp(realFlick.y, -1f, 1f);                
+                realFlick.y = Mathf.Clamp(realFlick.y, -1f, 1f);
                 Debug.Log(realFlick);
             }
 
+            // Esto dispararía el objeto
             if (flick.magnitude < 0.1f)
             {
                 Vector2 projectilePos = -realFlick.normalized;
@@ -72,19 +84,43 @@ public class PlayerScript : MonoBehaviour
                 Rigidbody2D projectileRb = aProjectile.GetComponent<Rigidbody2D>();
                 projectileRb.AddForce(-realFlick * projectileSpeed);
                 isHolding = false;
-            }            
-        } else {
-            if (flick.magnitude > 0.1f){
-                isHolding = true;
-                realFlick = flick.normalized;
+
+                cooldown = maxCooldown;
             }
         }
-        
+        else
+        {
+            if (flick.magnitude > 0.1f)
+            {
+                isHolding = true;
+                realFlick = flick;
+            }
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D col){
-        if (col.gameObject.CompareTag("piso")){
-            canJump = true;
+    void move()
+    {
+        float movement = Input.GetAxis("P" + id.ToString() + "Horizontal");
+        if (Mathf.Abs(movement) > 0.5)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rb.transform.position, new Vector2(movement, 0), 1f, solidMask.value);
+            if (!hit)
+            {                
+                rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+            }
+
+        }
+    }
+
+    void jump()
+    {
+        if (canJump)
+        {
+            if (Input.GetButton("P" + id.ToString() + "Jump"))
+            {
+                rb.AddForce(new Vector2(0f, jumpForce));
+                canJump = false;
+            }
         }
     }
 }
