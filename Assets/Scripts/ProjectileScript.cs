83c4c575777534122392dc71a45d51263f23d5d5
Dagian <fjprevi@gmail.com>
2019-06-08 14:56:59 -0300

    added proyectile destruction

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
+    public Rigidbody2D theBody;
+    float initialTime;
+    float lifeSpam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialTime = Time.time;
+        lifeSpam = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time - initialTime > lifeSpam)
+        {
+            Destroy(gameObject);
+        }
     }
 }
