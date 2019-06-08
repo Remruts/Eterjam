1dbd61aa52263f5bdf221c1a331ca95c58afcd98
Giuliano Francisco Vitelli <giuli@Giulianos-MacBook-Pro.local>
2019-06-08 19:14:49 -0300

    sprites

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     Rigidbody2D rb;
+<<<<<<< Updated upstream
     public float speed = 30f;
+=======
+    Animator animator;
+    bool isHolding = false;
+
+    public float speed = 5f;
+    public float maxSpeed = 5f;
+    public float projectileSpeed = 300f;
+    public float jumpForce = 300f;
+
+    public float maxCooldown = 1f;
+
+>>>>>>> Stashed changes
     bool canJump = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,9 +46,38 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
     void OnCollisionEnter2D(Collision2D col){
         if (col.gameObject.CompareTag("piso")){
             canJump = true;
+=======
+    void move()
+    {
+        float movement = Input.GetAxis("P" + id.ToString() + "Horizontal");
+        if (Mathf.Abs(movement) > 0.5)
+        {
+            animator.SetBool("standing", false);
+            RaycastHit2D hit = Physics2D.Raycast(rb.transform.position, new Vector2(movement, 0), 1f, solidMask.value);
+            if (!hit)
+            {                
+                rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+            }
+
+        } else {
+            animator.SetBool("standing", true);
+        }
+    }
+
+    void jump()
+    {
+        if (canJump)
+        {
+            if (Input.GetButton("P" + id.ToString() + "Jump"))
+            {
+                rb.AddForce(new Vector2(0f, jumpForce));
+                canJump = false;
+            }
+>>>>>>> Stashed changes
         }
     }
 }
