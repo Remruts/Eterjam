259aef86fa1ea0d24a065fd65c9c201ac6952ec7
m4r70 <martinjaltman@gmail.com>
2019-06-08 13:51:32 -0300

    playerScript WIP

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float speed = 30f;
+    bool canJump = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float movement = Input.GetAxis("P1Horizontal");
+        if (Mathf.Abs(movement) > 0.5)
+        {
+            rb.MovePosition(rb.transform.position + new Vector3(movement * speed * Time.deltaTime, 0f, 0f));
+        }
+
+        if (canJump)
+        {
+            if (Input.GetButton("P1Jump"))
+            {
+                rb.AddForce(Vector3.up * speed);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if (col.gameObject.CompareTag("piso")){
+            canJump = true;
+        }
     }
 }
