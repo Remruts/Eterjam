eff06964d025609d47e77503c44042af8ba42069
Dagian <fjprevi@gmail.com>
2019-06-08 15:44:00 -0300

    first Manager

--- Assets/Scripts/ManagerScript.cs
+++ Assets/Scripts/ManagerScript.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.SceneManager;
 
 public class ManagerScript : MonoBehaviour
 {
+    //global variables
+
+    // player array
+
+    ArrayList currentPlayers;
+
+    int bodyCount;
+    
     // Start is called before the first frame update
     void Start()
     {
+        // Spawns players, player arrays?
+         
         
     }
 
@@ -15,4 +26,45 @@ public class ManagerScript : MonoBehaviour
     {
         
     }
+
+
+    public void onPlayerDeath(PlayerScript aDeadPlayer)
+    {
+        
+        currentPlayers.Remove(aDeadPlayer);
+        int deadPlayerTeam = aDeadPlayer.team;
+        foreach (PlayerScript player in this.currentPlayers)
+        {
+            if(player.team == deadPlayerTeam)
+            {
+                return;
+            }
+        }
+        this.roundOver(deadPlayerTeam + 1 % 2);
+    }
+
+    public void pauseGame()
+    {
+        // pause the game TODO
+    }
+    
+
+   void roundOver(int aWinningTeam)
+    {
+
+        //SceneManager.loadScene(SceneManager.GetActiveScene().name);
+        // Do something
+
+        // show congrats()
+
+    }
+    
+
+    void addPlayer()
+    {
+
+    }
+
+
 }
+
