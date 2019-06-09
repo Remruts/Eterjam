e894572dd6eac364cb303b2599180d0a6fd6ebcc
Andy <remruts@gmail.com>
2019-06-09 01:56:53 -0300

    AAAAAAAAH ESTOy- ebriO, pero mirà que no soy re funcional y te arreglo todo

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,72 +4,79 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    Rigidbody2D rb;
-    bool isHolding = false;
-    Animator animator;
+  Rigidbody2D rb;
+  bool isHolding = false;
 
-    public float speed = 5f;
-    public float maxSpeed = 5f;
-    public float projectileSpeed = 300f;
-    public float jumpForce = 300f;
+  public float speed = 5f;
+  public float maxSpeed = 5f;
+  public float projectileSpeed = 300f;
+  public float jumpForce = 300f;
+	public float dashForce = 600f;
+  public float maxCooldown = 1f;
+	public float maxDashCooldown = 1f;
 
-    public float maxCooldown = 1f;
+  bool canJump = false;
+	bool isDashing = false;
+	float dashCooldown = 0f;
+  public LayerMask solidMask;
 
-    bool canJump = false;
-    public LayerMask solidMask;
+  public GameObject projectilePrefab;
+	public RectTransform forceBar;
+  public int team = 0;
+  public int id = 0;
 
-    public GameObject projectilePrefab;
-    public RectTransform forceBar;
-    public int team = 0;
+  public float flickSpeed = 1f;
+  Vector2 realFlick = Vector2.zero;
+  float cooldown = 0f;
 
-    public int id = 0;
+	float movement = 0;
 
-    public float flickSpeed = 1f;
-
-    Vector2 realFlick = Vector2.zero;
-
-    float cooldown = 0f;
+	Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        ManagerScript.coso.addPlayer(this);
-        animator = GetComponent<Animator>();
-
+				if (ManagerScript.coso != null){
+					ManagerScript.coso.addPlayer(this);
+				}
+				anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        move();
-        jump();
-        if (cooldown == 0f)
-        {
-            shoot();
-        }
-        else if (cooldown > 0f)
-        {
-            cooldown -= Time.deltaTime;
-            if (cooldown < 0f)
-            {
-                cooldown = 0f;
-            }
-        }
+				if (!isDashing){
+					move();
+	        jump();
+					if (cooldown == 0f) {
+	            shoot();
+	        } else if (cooldown > 0f)
+	        {
+	            cooldown -= Time.deltaTime;
+	            if (cooldown < 0f)
+	            {
+	                cooldown = 0f;
+	            }
+	        }
+				}
 
-        if (Mathf.Abs(rb.velocity.x) > maxSpeed)
-            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+				dash();
 
+				rb.velocity = new Vector2(movement * speed + (isDashing ? dashForce * Mathf.Sign(transform.localScale.x) : 0f), rb.velocity.y);
+
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed + (isDashing ? dashForce : 0f))
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed + (isDashing ? dashForce * Mathf.Sign(transform.localScale.x)  : 0f), rb.velocity.y);
     }
 
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("piso"))
-        {
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.CompareTag("piso")) {
+						var state = anim.GetCurrentAnimatorStateInfo(0);
+						if (state.IsName("Jump")){
+							anim.Play("Idle");
+						}
             canJump = true;
-        }
-        else if (col.gameObject.CompareTag("projectile"))
-        {
+        } else if (col.gameObject.CompareTag("projectile")){
             Debug.Log("stuff");
             ManagerScript.coso.onPlayerDeath(this);
             Destroy(gameObject);
@@ -79,13 +86,17 @@ public class PlayerScript : MonoBehaviour
     void shoot()
     {
         // Shooting
-        Vector2 flick = new Vector2(Input.GetAxisRaw("P" + id.ToString() + "FlickX"), -Input.GetAxisRaw("P" + id.ToString() + "FlickY"));
+        Vector2 flick = new Vector2(Input.GetAxisRaw("P" + id.ToString() +"FlickX"), -Input.GetAxisRaw("P" + id.ToString() + "FlickY"));
 
         if (isHolding)
         {
-            Vector2 forceBarPos = forceBar.anchoredPosition;
-            forceBarPos.x = -1.5f + 1.5f * realFlick.magnitude / 4.24f;
-            forceBar.anchoredPosition = forceBarPos;
+						Vector3 prevScale = transform.localScale;
+						prevScale.x = -Mathf.Sign(-flick.x);
+						transform.localScale = prevScale;
+
+						Vector2 forceBarPos = forceBar.anchoredPosition;
+						forceBarPos.x = -1.5f + 1.5f * realFlick.magnitude/4.24f;
+						forceBar.anchoredPosition = forceBarPos;
 
             if (flick.magnitude > 0.1f)
             {
@@ -104,7 +115,7 @@ public class PlayerScript : MonoBehaviour
                 isHolding = false;
 
                 cooldown = maxCooldown;
-                forceBar.parent.gameObject.SetActive(false);
+								forceBar.parent.gameObject.SetActive(false);
             }
         }
         else
@@ -113,40 +124,55 @@ public class PlayerScript : MonoBehaviour
             {
                 isHolding = true;
                 realFlick = flick;
-                forceBar.parent.gameObject.SetActive(true);
+								forceBar.parent.gameObject.SetActive(true);
             }
         }
     }
 
     void move()
     {
-        float movement = Input.GetAxis("P" + id.ToString() + "Horizontal");
+        movement = Input.GetAxis("P" + id.ToString() + "Horizontal");
         if (Mathf.Abs(movement) > 0.5)
         {
-            animator.SetBool("standing", false);
-
-            RaycastHit2D hit = Physics2D.Raycast(rb.transform.position, new Vector2(movement, 0), 1f, solidMask.value);
-            if (!hit)
+						anim.SetBool("standing", false);
+            RaycastHit2D hit = Physics2D.Raycast(rb.transform.position, new Vector2(movement, 0), 0.5f, solidMask.value);
+            if (hit)
             {
-                rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+                movement = 0f;
             }
-
-        }
-        else
-        {
-            animator.SetBool("standing", true);
-
-        }
+        } else {
+					anim.SetBool("standing", true);
+				}
     }
-        void jump()
+
+    void jump()
     {
         if (canJump)
         {
             if (Input.GetButton("P" + id.ToString() + "Jump"))
             {
+								anim.Play("Jump");
                 rb.AddForce(new Vector2(0f, jumpForce));
                 canJump = false;
             }
         }
     }
+
+		void dash(){
+			dashCooldown -= Time.deltaTime;
+
+			if (dashCooldown <= maxDashCooldown - 0.1f){
+					isDashing = false;
+			}
+			if (dashCooldown <= 0f){
+				dashCooldown = 0f;
+			}
+
+			if (dashCooldown <= 0f){
+				if (Input.GetButton("P" + id.ToString() + "Dash")){
+					isDashing = true;
+					dashCooldown = maxDashCooldown;
+				}
+			}
+		}
 }
