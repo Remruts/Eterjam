5e38f4593a2b1dfc61435d31c7935cfb2277f54f
remruts <remruts@gmail.com>
2019-10-14 14:40:33 -0300

    Con manager de audio

--- Assets/Scripts/ManagerScript.cs
+++ Assets/Scripts/ManagerScript.cs
@@ -5,96 +5,102 @@ using UnityEngine.SceneManagement;
 using TMPro;
 
 public enum typeOfMatch{
-    vidas = 0,
-    tiempo = 1
+  vidas = 0,
+  tiempo = 1
 }
 
 public class ManagerScript : MonoBehaviour
 {
-    public int startingLives = 3;
-    public float startingTime = 120f;
-    public float maxStartingTime = 900f;
-    public float minStartingTime = 30f;
+  public int startingLives = 3;
+  public float startingTime = 120f;
+  public float maxStartingTime = 900f;
+  public float minStartingTime = 30f;
 
-    public int minStartingLives = 1;
-    public int maxStartingLives = 99;
-    
-    public typeOfMatch matchType = typeOfMatch.vidas;
-    public static ManagerScript coso;
+  public int minStartingLives = 1;
+  public int maxStartingLives = 99;
+  
+  public typeOfMatch matchType = typeOfMatch.vidas;
+  public static ManagerScript coso;
 
-    public Texture2D[] transBacks;
+  public Texture2D[] transBacks;
 
-    public int currentWinningTeam = 0;
+  public int currentWinningTeam = 0;
 
-    int[] results;
+  int[] results;
 
-    public string resultsScene = "endBattleScene";
-    public string matchSettingsScene = "matchSettingsScene";
-    public string battleScene = "SpritesScene";
-    public string titleScene = "TitleScene";
+  public string resultsScene = "endBattleScene";
+  public string matchSettingsScene = "matchSettingsScene";
+  public string battleScene = "SpritesScene";
+  public string titleScene = "TitleScene";
 
-    CustomTimer timeScaleTimer;
-   
-    // Start is called before the first frame update
-    void Awake(){
-      // TODO: eliminar al manager de la escena
-      if (ManagerScript.coso == null){
-        DontDestroyOnLoad(gameObject);
-        ManagerScript.coso = this;
-      } else{
-        Destroy(gameObject);
+  [Range(0f, 1f)]
+  public float musicVolume = 1.0f;
+  [Range(0f, 1f)]
+  public float audioVolume = 1.0f;
+
+  CustomTimer timeScaleTimer;
+  
+  // Start is called before the first frame update
+  void Awake(){
+    // TODO: eliminar al manager de la escena
+    if (ManagerScript.coso == null){
+      DontDestroyOnLoad(gameObject);
+      ManagerScript.coso = this;
+    } else{
+      Destroy(gameObject);
+    }
+  }
+
+  public void saveResults(int team0, int team1){
+    results = new int[2];
+    results[0] = team0;
+    results[1] = team1;
+  }
+
+  // Update is called once per frame
+  void Update(){
+    if (SceneManager.GetActiveScene().name == resultsScene){
+      if(Input.GetButtonDown("P1Jump") || Input.GetButtonDown("P2Jump") || Input.GetButtonDown("P1Start") || Input.GetButtonDown("P2Start")) {
+        goTo(titleScene);
       }
     }
-
-    public void saveResults(int team0, int team1){
-      results = new int[2];
-      results[0] = team0;
-      results[1] = team1;
+    if (Input.GetKey("escape")){
+      Application.Quit();
     }
+  }
 
-    // Update is called once per frame
-    void Update(){
-      if (SceneManager.GetActiveScene().name == resultsScene){
-        if(Input.GetButtonDown("P1Jump") || Input.GetButtonDown("P2Jump") || Input.GetButtonDown("P1Start") || Input.GetButtonDown("P2Start")) {
-            goTo(titleScene);
-        }
-      }
-      if (Input.GetKey("escape")){
-        Application.Quit();
-      }
-    }
+  public void endMatch(){
+    Invoke(nameof(resetTimeScale), 0.1f);
+    goTo(resultsScene);
+  }
 
-    public void endMatch(){
-        Invoke(nameof(resetTimeScale), 0.1f);
-        goTo(resultsScene);
-    }
+  public void goTo(string scene){
+    audioManager.audioman.PlayMusicOfScene(scene, 0.5f);
+    transitionScript.transition.setTransition(scene, "noise0");
+    transitionScript.transition.startTransition(0.5f);
+    transitionScript.transition.setTransitionTexture(transBacks[currentWinningTeam]);
+  }
 
-    public void goTo(string scene){
-      transitionScript.transition.setTransition(scene, "noise0");
-      transitionScript.transition.startTransition(0.5f);
-      transitionScript.transition.setTransitionTexture(transBacks[currentWinningTeam]);
-    }
+  void resetTimeScale(){
+    Time.timeScale = 1f;
+  }
 
-    void resetTimeScale(){
-      Time.timeScale = 1f;
+  public void addStartingTime(float t){
+    startingTime += t;
+    if (startingTime > maxStartingTime){
+      startingTime = minStartingTime;
+    } else if (startingTime < minStartingTime){
+      startingTime = maxStartingTime;
     }
+  }
 
-    public void addStartingTime(float t){
-      startingTime += t;
-      if (startingTime > maxStartingTime){
-        startingTime = minStartingTime;
-      } else if (startingTime < minStartingTime){
-        startingTime = maxStartingTime;
-      }
+  public void addStartingLives(int l){
+    startingLives += l;
+    if (startingLives > maxStartingLives){
+      startingLives = minStartingLives;
+    } else if (startingLives < minStartingLives){
+      startingLives = maxStartingLives;
     }
-
-    public void addStartingLives(int l){
-      startingLives += l;
-      if (startingLives > maxStartingLives){
-        startingLives = minStartingLives;
-      } else if (startingLives < minStartingLives){
-        startingLives = maxStartingLives;
-      }
-    }
+  }
 
 }
--- Assets/Scripts/audioManager.cs
+++ Assets/Scripts/audioManager.cs
@@ -3,17 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class audioManager : MonoBehaviour
-{
-    public static audioManager audioman;
-    // Por ahora sólo sigue andando la música
-    void Awake(){
-        if (audioman == null){
-            DontDestroyOnLoad(this.gameObject);
-            audioman = this;
-        }
-        else{
-            Destroy(gameObject);
-            return;
-        }
+  {
+  public static audioManager audioman;
+  public float fadeTime = 1.0f;
+
+  AudioSource music;
+  
+  public Music[] trackList;
+  public Dictionary<string, Music> trackOfScene;
+
+  AudioClip nextTrack;
+  musicPlayState state = musicPlayState.fadeIn;
+
+  float musicVolume = 0f;
+  float maxVolume = 1.0f;
+  
+  // Por ahora sólo sigue andando la música
+  void Awake(){
+    if (audioman == null){
+      DontDestroyOnLoad(this.gameObject);
+      audioman = this;
+      trackOfScene = new Dictionary<string, Music>();
+			foreach (Music m in trackList){
+				trackOfScene.Add(m.scene, m);
+			}
+    } else {
+      Destroy(gameObject);
+      return;
     }
+
+    music = GetComponent<AudioSource>();
+  }
+
+  void Update(){
+    maxVolume = ManagerScript.coso.musicVolume;
+    
+    switch(state){
+    case musicPlayState.fadeOut:
+      fadeOut();
+    break;
+    case musicPlayState.fadeIn:
+      fadeIn();
+    break;
+    }
+
+    music.volume = musicVolume;
+  }
+
+  public void PlayMusicOfScene(string scene, float timeToFade = 1.0f){
+    nextTrack = trackOfScene[scene].track;
+    state = musicPlayState.fadeOut;
+    fadeTime = timeToFade;
+  }
+
+  void fadeIn(){
+    if (musicVolume < maxVolume){
+      musicVolume += Time.deltaTime / fadeTime;
+    } else {
+      musicVolume = maxVolume;
+      state = musicPlayState.playing;
+    }
+  }
+
+  void fadeOut(){
+    if (musicVolume > 0f){
+      musicVolume -= Time.deltaTime / fadeTime;
+    } else {
+      musicVolume = 0f;
+      state = musicPlayState.fadeIn;
+      if (nextTrack != null){
+        music.clip = nextTrack;
+        music.Play();
+      }
+    }
+  }
 }
+
+public enum musicPlayState{
+  playing = 0,
+  fadeIn = 1,
+  fadeOut = 2
+}
+
+[System.Serializable]
+public class Music {
+  public string scene;
+  public AudioClip track;
+
+  public Music(string theScene, AudioClip theTrack){
+    scene = theScene;
+    track = theTrack;
+  }
+};
