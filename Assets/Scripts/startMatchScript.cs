929a2410a89cbed96c917d906999e3d849021da8
remruts <remruts@gmail.com>
2019-10-17 17:08:14 -0300

    loop de settings e ir atrás en settings

--- Assets/Scripts/startMatchScript.cs
+++ Assets/Scripts/startMatchScript.cs
@@ -8,5 +8,8 @@ public class startMatchScript : MonoBehaviour
     if (Input.GetButtonDown("P1Start") || Input.GetButtonDown("P2Start")){
       ManagerScript.coso.goTo(ManagerScript.coso.battleScene);
     }
+    if (Input.GetButtonDown("P1Dash") || Input.GetButtonDown("P2Dash")){
+      ManagerScript.coso.goTo(ManagerScript.coso.titleScene);
+    }
   }
 }
